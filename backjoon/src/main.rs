7e97ddd52895c6807a735c232740beb3e9c20f42
PracticeEveryday <developerforus@gmail.com>
2023-10-17 19:07:41 +0900

    BAEKJOON: 1000 스네이크케이스로 변경

--- backjoon/src/main.rs
+++ backjoon/src/main.rs
@@ -1,31 +1,31 @@
 use std::io;
 
-// fn main() {
-//     let mut numbersArr = String::new();
-//     dbg!(&numbersArr);
-//     io::stdin().read_line(&mut numbersArr).unwrap();
-    
-//     let numbers: Vec<&str> = numbersArr.split_whitespace().collect();
-//     dbg!(&numbers);
-    
-//     let number_first = numbers[0].parse::<i32>().unwrap();
-//     let number_second = numbers[1].parse::<i32>().unwrap();
-
-//     println!("{}", number_first + number_second);
-// }
-
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let number_arr:Vec<&str> = input.split_whitespace().collect();
+    let mut numbers_arr = String::new();
+    dbg!(&numbers_arr);
+    io::stdin().read_line(&mut numbers_arr).unwrap();
     
-    let a: i32 = number_arr[0].trim().parse().unwrap();
-    let b: i32 = number_arr[1].trim().parse().unwrap();
+    let numbers: Vec<&str> = numbers_arr.split_whitespace().collect();
+    dbg!(&numbers);
+    
+    let number_first = numbers[0].parse::<i32>().unwrap();
+    let number_second = numbers[1].parse::<i32>().unwrap();
 
-    // dbg!(number_arr);
-    // dbg!(a);
-    // dbg!(b);
-
-     println!("{}", a - b);
+    println!("{}", number_first + number_second);
 }
+
+// fn main() {
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).unwrap();
+
+//     let number_arr:Vec<&str> = input.split_whitespace().collect();
+    
+//     let a: i32 = number_arr[0].trim().parse().unwrap();
+//     let b: i32 = number_arr[1].trim().parse().unwrap();
+
+//     // dbg!(number_arr);
+//     // dbg!(a);
+//     // dbg!(b);
+
+//      println!("{}", a - b);
+// }
