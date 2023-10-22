261f8a7d2790a055ddfb09f695b41cc6afe20753
PracticeEveryday <developerforus@gmail.com>
2023-10-22 10:45:23 +0900

    BAEKJOON a*b

--- backjoon/src/main.rs
+++ backjoon/src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
 
-fn main() {
-    let mut numbers_arr = String::new();
-    dbg!(&numbers_arr);
-    io::stdin().read_line(&mut numbers_arr).unwrap();
+// fn main() {
+//     let mut numbers_arr = String::new();
+//     dbg!(&numbers_arr);
+//     io::stdin().read_line(&mut numbers_arr).unwrap();
     
-    let numbers: Vec<&str> = numbers_arr.split_whitespace().collect();
-    dbg!(&numbers);
+//     let numbers: Vec<&str> = numbers_arr.split_whitespace().collect();
+//     dbg!(&numbers);
     
-    let number_first = numbers[0].parse::<i32>().unwrap();
-    let number_second = numbers[1].parse::<i32>().unwrap();
+//     let number_first = numbers[0].parse::<i32>().unwrap();
+//     let number_second = numbers[1].parse::<i32>().unwrap();
 
-    println!("{}", number_first + number_second);
-}
+//     println!("{}", number_first + number_second);
+// }
 
 // fn main() {
 //     let mut input = String::new();
@@ -29,3 +29,17 @@ fn main() {
 
 //      println!("{}", a - b);
 // }
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // dbg!(&input);
+    let arr:Vec<&str> = input.split_whitespace().collect();
+    // dbg!(&arr);
+
+    let a: i32 = arr[0].trim().parse().unwrap();
+    let b: i32 = arr[1].trim().parse().unwrap();
+
+    println!("{}", a*b);
+}
