eea784893ec169375124227113db5a9f0f50935d
PracticeEveryday <developerforus@gmail.com>
2023-10-22 23:30:43 +0900

    BEAKJOON 고양이까지!

--- backjoon/src/main.rs
+++ backjoon/src/main.rs
@@ -112,18 +112,71 @@ use std::io;
 //     println!("{}", (a%c)*(b%c)%c);
 // }
 
+// fn main() {
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).expect("please input");
+
+//     let arr: Vec<i32> = input.split_whitespace()
+//         .map(|s| s.trim().parse().expect("Failed to parse input"))
+//         .collect();
+
+//     let (a, b, c) = (arr[0], arr[1], arr[2]);
+
+//     println!("{}", (a + b) % c);
+//     println!("{}", ((a % c) + (b % c)) % c);
+//     println!("{}", (a * b) % c);
+//     println!("{}", (a % c) * (b % c) % c);
+// }
+
+
+// fn main() {
+//     let mut input = String::new();
+    
+
+//     for _ in 0..2 {
+//         io::stdin().read_line(&mut input).expect("pleas input");
+//     }
+
+//     let arr :Vec<u32> = input.split_whitespace()
+//         .map(|s| s.trim().parse().expect("파싱에 문제가 있습니다."))
+//         .collect();
+
+//     let num_1 = arr[0];
+//     let num_2 = arr[1];
+
+//     let num_2_str = num_2.to_string();
+//     let num_2_str_arr: Vec<char> = num_2_str.chars().collect();
+//     let num_2_num_arr:Vec<u32> = num_2_str_arr.iter().map(|s| s.to_digit(10).expect("숫자로 파싱합니다.")).collect();
+
+//     let reversed_result: Vec<u32> = num_2_num_arr.into_iter().rev().collect(); // 벡터를 뒤집음
+
+//     reversed_result.iter().for_each(|s| println!("{}", s * num_1));
+//     println!("{}", num_1 * num_2);
+// }
+
+
+// fn main () {
+//     let mut input = String::new();
+
+//     io::stdin().read_line(&mut input).expect("인풋에 실패하였습니다.");
+
+//     let arr: Vec<u64> = input.split_whitespace().map(|s| s.trim().parse().expect("파싱에러")).collect();
+//     let mut result:u64 = 0;
+//     for i in arr {
+//         result += i;
+//     }
+
+//     println!("{}", result);
+// }
+
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("please input");
-
-    let arr: Vec<i32> = input.split_whitespace()
-        .map(|s| s.trim().parse().expect("Failed to parse input"))
-        .collect();
-
-    let (a, b, c) = (arr[0], arr[1], arr[2]);
-
-    println!("{}", (a + b) % c);
-    println!("{}", ((a % c) + (b % c)) % c);
-    println!("{}", (a * b) % c);
-    println!("{}", (a % c) * (b % c) % c);
+    println!("{0}    {1}{0}", "\\", "/");
+    println!(" {0}  {1} {2}{0}", ")", "(", "'");
+    println!("{0}  {1}  {2}", "(", "/", ")");
+    println!(" {0}{1}{2}{2}{3}{4}", "\\", "(", "_", ")", "|",);
 }
+
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
