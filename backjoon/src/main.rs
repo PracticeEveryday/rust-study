c4b25ae4ed7294a1bc1f1abaad94e3899bf3c02e
PracticeEveryday <developerforus@gmail.com>
2023-10-22 12:20:30 +0900

    BEAKJOON ??!

--- backjoon/src/main.rs
+++ backjoon/src/main.rs
@@ -30,16 +30,55 @@ use std::io;
 //      println!("{}", a - b);
 // }
 
-fn main() {
+// fn main() {
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).unwrap();
+
+//     // dbg!(&input);
+//     let arr:Vec<&str> = input.split_whitespace().collect();
+//     // dbg!(&arr);
+
+//     let a: i32 = arr[0].trim().parse().unwrap();
+//     let b: i32 = arr[1].trim().parse().unwrap();
+
+//     println!("{}", a*b);
+// }
+
+
+// fn main() {
+//     let mut input = String::new();
+
+//     io::stdin().read_line(&mut input).unwrap();
+
+//     let arr: Vec<&str> = input.split_whitespace().collect();
+    
+//     let a :f64 = arr[0].trim().parse().unwrap();
+//     let b :f64 = arr[1].trim().parse().unwrap();
+
+//     println!("{}", a / b);
+// }
+
+// fn main () {
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).unwrap();
+
+//     let arr:Vec<&str> = input.split_whitespace().collect();
+
+//     let a :i32 = arr[0].parse().unwrap();
+//     let b :i32 = arr[1].parse().unwrap();
+
+//     println!("{}", a + b);
+//     println!("{}", a - b);
+//     println!("{}", a * b);
+//     println!("{}", a / b);
+//     println!("{}", a % b);
+// }
+
+fn main () {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    // dbg!(&input);
-    let arr:Vec<&str> = input.split_whitespace().collect();
-    // dbg!(&arr);
+    let trimed = input.trim();
 
-    let a: i32 = arr[0].trim().parse().unwrap();
-    let b: i32 = arr[1].trim().parse().unwrap();
-
-    println!("{}", a*b);
+    println!("{}??!", trimed);
 }
