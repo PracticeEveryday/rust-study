5b568bdfea04719791022a05c9b755762e312811
PracticeEveryday <developerforus@gmail.com>
2023-11-01 17:02:13 +0900

    BAEKJOON 20231101

--- baekjoon/src/main.rs
+++ baekjoon/src/main.rs
@@ -44,50 +44,97 @@
 //     println!("{}", result_str);
 // }
 
-fn main () {
-  // 9 X 9 행렬이 주어질 때 최대값과 위치를 구하라!
-  let mut procession_arr:Vec<Vec<usize>> = Vec::new();
+// fn main () {
+//   // 9 X 9 행렬이 주어질 때 최대값과 위치를 구하라!
+//   let mut procession_arr:Vec<Vec<usize>> = Vec::new();
 
 
-  for _ in 0..9 {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("check input value");
+//   for _ in 0..9 {
+//     let mut input = String::new();
+//     std::io::stdin().read_line(&mut input).expect("check input value");
     
-    // 9x9 행렬 생성
-    let input_arr: Vec<usize> = input.split_whitespace().map(|val| val.trim().parse().expect("check parsed value")).collect();
-    procession_arr.push(input_arr);
+//     // 9x9 행렬 생성
+//     let input_arr: Vec<usize> = input.split_whitespace().map(|val| val.trim().parse().expect("check parsed value")).collect();
+//     procession_arr.push(input_arr);
 
-    input.clear();
-  }
+//     input.clear();
+//   }
 
-  let mut max_val = 0;
-  let mut max_val_coordinate = String::new();
+//   let mut max_val = 0;
+//   let mut max_val_coordinate = String::new();
 
-  for (key, vec) in procession_arr.iter().enumerate() {
-    for (key_depth_two, _) in vec.iter().enumerate() {
-      if max_val <= procession_arr[key][key_depth_two] {
-        max_val = procession_arr[key][key_depth_two];
-        max_val_coordinate.clear();
-        max_val_coordinate.push_str(&format!("{} {}", key + 1, key_depth_two + 1));
+//   for (key, vec) in procession_arr.iter().enumerate() {
+//     for (key_depth_two, _) in vec.iter().enumerate() {
+//       if max_val <= procession_arr[key][key_depth_two] {
+//         max_val = procession_arr[key][key_depth_two];
+//         max_val_coordinate.clear();
+//         max_val_coordinate.push_str(&format!("{} {}", key + 1, key_depth_two + 1));
+//       }
+//     }
+//   }
+
+//   println!("{}", max_val);
+//   println!("{}", max_val_coordinate);
+
+//   /*
+//   3 23 85 34 17 74 25 52 65
+//   10 7 39 42 88 52 14 72 63
+//   87 42 18 78 53 45 18 84 53
+//   34 28 64 85 12 16 75 36 55
+//   21 77 45 35 28 75 90 76 1
+//   25 87 65 15 28 11 37 28 74
+//   65 27 75 41 7 89 78 64 39
+//   47 47 70 45 23 65 3 41 44
+//   87 13 82 38 31 12 29 29 90
+
+//   90
+//   5 7  
+//    */
+// }
+
+fn main () {
+  // 세로 읽기
+  // 빈칸은 지나치고 읽으라!
+  let mut input = String::new();
+  let mut vec_hashmap: std::collections::HashMap<usize, String> = std::collections::HashMap::new();
+
+  for _ in 0..5 {
+    std::io::stdin().read_line(&mut input).expect("check input value");
+    let mut input_str = input.trim().to_string();
+    
+    while input_str.len() == 6 {
+      input_str.push('*')
+    }
+
+    for (key, c) in input_str.chars().enumerate() {
+      // 가변 참조로 얻기..
+      if let Some(count) = vec_hashmap.get_mut(&key) {
+        // hashmap이 있으면 
+        count.push(c);
+      } else {
+        // 없으면
+        vec_hashmap.insert(key, String::from(c));
       }
     }
+    input.clear();
+  }
+  let mut result_str = String::new();
+
+  // dbg!(&vec_hashmap);
+  for key in 0..20 {
+      if let Some(value) = vec_hashmap.get(&key) {
+          result_str.push_str(value);
+      }
   }
 
-  println!("{}", max_val);
-  println!("{}", max_val_coordinate);
-
+  println!("{}", result_str.replace("*", ""));
   /*
-  3 23 85 34 17 74 25 52 65
-  10 7 39 42 88 52 14 72 63
-  87 42 18 78 53 45 18 84 53
-  34 28 64 85 12 16 75 36 55
-  21 77 45 35 28 75 90 76 1
-  25 87 65 15 28 11 37 28 74
-  65 27 75 41 7 89 78 64 39
-  47 47 70 45 23 65 3 41 44
-  87 13 82 38 31 12 29 29 90
-
-  90
-  5 7  
-   */
+  AABCDD
+  afzz
+  09121
+  a8EWg6
+  P5h3kx
+  
+  Aa0aPAf985Bz1EhCz2W3D1gkD6x
+   */ 
 }
